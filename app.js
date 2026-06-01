3268ffa4d395bac11d98db1a614cba3d7179c21d
Nasiroddin khatib <nasirkhatib01@gmail.com>
2026-06-01 04:47:47 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API = 'http://Backend-LB-1242084244.ap-south-1.elb.amazonaws.com';
+const API = 'http://Backend-LB-33015680.ap-south-1.elb.amazonaws.com';
 
 
 // ================= REGISTER =================
